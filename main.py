aceabe7bc18ccd383760dce33ef45bc643fbd5f2
FranciscoDadone <dadonefrancisco@hotmail.com.ar>
2021-05-20 21:58:28 -0300

    bug fix

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ try:
     from SwSpotify import spotify
 except:
     print("Install SwSpotify using: ", "pip install SwSpotify")
-import time
+import time, os
 
 from SwSpotify import SpotifyClosed, SpotifyNotRunning, SpotifyPaused
 
@@ -11,18 +11,18 @@ def mute(mute):
         os.mkdir("tmp")
     except: pass
     os.system("pacmd list-sink-inputs > tmp/list-sink-inputs")
-    process_id = 0
+    process_id = []
     with open("tmp/list-sink-inputs", "r") as file:
+        wasFound = False
         for line in file:
-            wasFound = False
             stripped_line = line.strip()
             if(stripped_line[:7] == "index: "):
-                process_id = stripped_line[7:]
+                process_id.append(stripped_line[7:])
             if(stripped_line == 'media.name = "Spotify"' or stripped_line == 'application.name = "Spotify"'): 
                 wasFound = True
-                break
     if wasFound:
-        os.system(f"pacmd set-sink-input-mute {process_id} {mute}")
+        for process in process_id:
+            os.system(f"pacmd set-sink-input-mute {process} {mute}")
 
 def main():
     muted = False
